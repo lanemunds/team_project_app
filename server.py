37ec8de7d6a26a0c74d35501dcda61d749c2f2e8
lanemunds <lanemunds@gmail.com>
2022-11-03 15:23:07 -0600

    part 4 through step 5

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
+from forms import TeamForm
 
 app = Flask(__name__)
+app.secret_key = "keep this secret"
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    team_form = TeamForm()
+    return render_template('home.html', team_form = team_form)
+
+@ app.route("/add-team", methods = ["POST"])
+def add_team():
+    team_form = TeamForm()
+    
+    if team_form.validate_on_submit():
+        print(team_form.team_name.data)
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
